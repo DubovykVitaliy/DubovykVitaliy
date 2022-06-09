af2ae99f0b6a8e5eddd717a4003c28bf55cdb042
Vitaliy <vedubovik@gmail.com>
2022-06-09 20:03:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+// alert('hello');
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('How old are you?',"");
+// console.log(typeof(answer),answer+5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('what is your name?','');
+// answers[1] = prompt('what is your lastname?','');
+// answers[2] = prompt('How old are you?','');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/4`);
+
+
+// const user = 'Ivan';
+
+// alert(`hi, ${user}`);
+
+let incr = 10,
+    decr = 10;
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 4 != 8);
+
+const isChecked = true;
+const isClose = false;
+
+console.log(!isChecked || isClose);
+
+
+// console.log ('hello');
+// let number = 5;
+// number = 10;
+// console.log (number);
+
+// const arr = ['a','b','c'];
+
+// arr[10] = 425;
+// console.log(arr);
+
+
+// const arrobj = {
+//     a: 'a',
+//     1: 'b',
+//     2: 'c',
+//     abc: {
+//         def: 'sed',
+//     }
+// };
+// const b = 'h';
+
+// arrobj.b = '123';
+// arrobj['h'] = '123';
+// // console.log(arr[1]);
+// console.log(arrobj['b']);
+// console.log(arrobj.b);
